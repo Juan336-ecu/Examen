1efbaef4128afc19dd452bcfbd6b75468153f1f0
Juan336-ecu <jcvelasco3@espe.edu.ec>
2020-09-08 14:41:18 -0500

    Suma dos numeros

--- src/Suma.java
+++ src/Suma.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
 
 public class Suma {
+	
+	public static void main(String[] args) {
+		
+		Scanner reader =  new Scanner(System.in);
+		
+		int numero1 = 0;
+		int numero2 = 0;
+		int resultado=0;
+			
+		System.out.println("Introduce el primer número:");			
+		numero1 = reader.nextInt();
+					
+		System.out.println("Introduce el segundo número:");
+		numero2 = reader.nextInt();
+
+		resultado = numero1+numero2;
+		
+		System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado);
+		
+		
+	}
+	
 
 }
